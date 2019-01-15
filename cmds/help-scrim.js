d18ea7d787ae80d1fc576857e5a325aebac11446
jermyfab2000 <jermyfab2000@gmail.com>
2019-01-15 01:30:55 -0600

    Update help-scrim.js

--- cmds/help-scrim.js
+++ cmds/help-scrim.js
@@ -7,7 +7,10 @@ module.exports.run = async (bot,message,args) => {
     The Message Channel Commands are !1m or !1m-duos or !1m-squads to inform in the chat that one minute is missing for the match
     !5m or !5m-duos or !5m-squads to inform in the chat that five minutes is missing for the match
     !10m or !10m-duos or !10m-squads to inform in the chat that ten minute is missing for the match
-	!notes: command for advertising or any poster needed , to configure it use the command !setnotes**`)
+	!notes: command for advertising or any poster needed , to configure it use the command !setnotes
+     !auto-start start automatic scrims without having to put any code
+     you can start them manually but to save time I already added that I did it automatically
+     to stop the automatic scrims you just have to put !auto-stop**`)
         
         
         }
